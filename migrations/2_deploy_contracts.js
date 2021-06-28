847445368f574f62a60accfecba276a2735a2a10
Andy Jiang <andy@mask.io>
2021-06-27 20:11:19 -0700

    Add truffle network config and scripts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -10,18 +10,50 @@ const ERC20EMPTYNAME = artifacts.require("ERC20EMPTYNAME");
 const Web3 = require('web3');
 const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
 
-module.exports = function(deployer, network, accounts) {
-    owner = accounts[0];
-    proxyAdmin = accounts[1];
-    bep20ProxyAdmin = accounts[2];
-    deployer.then(async () => {
-        await deployer.deploy(ERC20ABC);
-        await deployer.deploy(ERC20DEF);
-        await deployer.deploy(ERC20EMPTYSYMBOL);
-        await deployer.deploy(ERC20EMPTYNAME);
+const MASK_TOKEN_ADDR_MAINNET = "0x69af81e73a73b40adf4f3d4223cd9b1ece623074";
+const MASK_TOKEN_ADDR_ROPSTEN = "0x0f6d3ec17ad4be4641fff47b98d970a2845c1365";
 
-        await deployer.deploy(BEP20TokenImplementation);
-        await deployer.deploy(BSCSwapAgentImpl, BEP20TokenImplementation.address, "10000000000000000", bep20ProxyAdmin);
-        await deployer.deploy(ETHSwapAgentImpl, "10000000");
-    });
+module.exports = function(deployer, network, accounts) {
+    const maskManagerAccount = accounts[0];
+    console.log("network: " + network);
+
+    if (network === "ropsten" || network === "mainnet" || network === "mainnet-fork" || network === "rinkeby") {
+        let maskTokenAddr = MASK_TOKEN_ADDR_ROPSTEN;
+        if (network === "mainnet") {
+            maskTokenAddr = MASK_TOKEN_ADDR_MAINNET;
+        }
+        deployer.then(async () => {
+            await deployer.deploy(ETHSwapAgentImpl, "10000000");
+            const ETHSwapAgentContract = await ETHSwapAgentImpl.deployed();
+            console.log("registerSwapPairToBSC: " + maskTokenAddr);
+            console.log("ETH Agent Address: " + ETHSwapAgentContract.address);
+            // const tx = await ETHSwapAgentContract.registerSwapPairToBSC(maskTokenAddr, {from: maskManagerAccount});
+            // console.log("registerSwapPairToBSC hash: " + tx.tx);
+        });
+    }
+    else if (network === "bsc_test" || network === "bsc_mainnet") {
+        deployer.then(async () => {
+            await deployer.deploy(BEP20TokenImplementation);
+            await deployer.deploy(BSCSwapAgentImpl, BEP20TokenImplementation.address, "10000000000000000", maskManagerAccount);
+            console.log("BEP20TokenImplementation address: " + BEP20TokenImplementation.address);
+
+            const bscSwap = await BSCSwapAgentImpl.deployed();
+            console.log("BSC Agent Address: " + bscSwap.address);
+        });
+    }
+    else {
+        owner = accounts[0];
+        proxyAdmin = accounts[1];
+        bep20ProxyAdmin = accounts[2];
+        deployer.then(async () => {
+            await deployer.deploy(ERC20ABC);
+            await deployer.deploy(ERC20DEF);
+            await deployer.deploy(ERC20EMPTYSYMBOL);
+            await deployer.deploy(ERC20EMPTYNAME);
+
+            await deployer.deploy(BEP20TokenImplementation);
+            await deployer.deploy(BSCSwapAgentImpl, BEP20TokenImplementation.address, "10000000000000000", bep20ProxyAdmin);
+            await deployer.deploy(ETHSwapAgentImpl, "10000000");
+        });
+    }
 };
--- truffle-config.js
+++ truffle-config.js
@@ -17,8 +17,9 @@
  * phrase from a file you've .gitignored so it doesn't accidentally become public.
  *
  */
-
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
+const ethers = require("ethers");
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+require("dotenv").config();
 // const infuraKey = "fj4jll3k.....";
 //
 // const fs = require('fs');
@@ -43,37 +44,85 @@ module.exports = {
     // options below to some value.
     //
     development: {
-     host: "127.0.0.1",     // Localhost (default: none)
-     port: 8545,            // Standard Ethereum port (default: none)
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 8545,            // Standard Ethereum port (default: none)
       network_id: "*",       // Any network (default: none)
     },
 
     // Another network with more advanced options...
     // advanced: {
-      // port: 8777,             // Custom port
-      // network_id: 1342,       // Custom network
-      // gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
-      // gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
-      // from: <address>,        // Account to send txs from (default: accounts[0])
-      // websockets: true        // Enable EventEmitter interface for web3 (default: false)
+    // port: 8777,             // Custom port
+    // network_id: 1342,       // Custom network
+    // gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
+    // gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
+    // from: <address>,        // Account to send txs from (default: accounts[0])
+    // websockets: true        // Enable EventEmitter interface for web3 (default: false)
     // },
 
-    // Useful for deploying to a public network.
-    // NB: It's important to wrap the provider as a function.
-    // ropsten: {
-      // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-      // network_id: 3,       // Ropsten's id
-      // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-      // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-      // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-      // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
+    ropsten: {
+      provider: new HDWalletProvider(process.env.PRIV_KEY_TEST, "https://ropsten.infura.io/v3/" + process.env.INFURA_API_KEY),
+      network_id: 3,
+      gas: 5000000,
+      gasPrice: 5000000000, // 5 Gwei
+      skipDryRun: true
+    },
 
+    rinkeby: {
+      provider: new HDWalletProvider(process.env.PRIV_KEY_TEST, "https://rinkeby.infura.io/v3/" + process.env.INFURA_API_KEY),
+      network_id: 4,
+      gas: 5000000,
+      gasPrice: 5000000000, // 5 Gwei
+      skipDryRun: true
+    },
+
+    mainnet: {
+      provider: new HDWalletProvider(process.env.PRIV_KEY_DEPLOY, "https://mainnet.infura.io/v3/" + process.env.INFURA_API_KEY),
+      network_id: 1,
+      gas: 5000000,
+      gasPrice: ethers.utils.parseUnits('20', 'gwei'),
+      timeoutBlocks: 50000,
+    },
+
+    bsc_test: {
+      provider: new HDWalletProvider(process.env.PRIV_KEY_TEST, 'https://data-seed-prebsc-1-s1.binance.org:8545'),
+      network_id: 97,
+      gas: 5000000,
+      gasPrice: 25000000000,
+      // timeoutBlocks: 50000,
+    },
+
+    bsc_mainnet: {
+      provider: new HDWalletProvider(process.env.PRIV_KEY_DEPLOY, 'https://bsc-dataseed1.binance.org:443'),
+      network_id: 56,
+      gas: 5000000,
+      gasPrice: ethers.utils.parseUnits('5', 'gwei'),
+      // timeoutBlocks: 50000,
+    },
+
+    /*
+    bsc_test: {
+        url: 'https://data-seed-prebsc-1-s1.binance.org:8545',
+        accounts: project_secret.private_key_list,
+        chainId: 97,
+        // 25 Gwei, if too low, we would see "ProviderError: transaction underpriced"
+        gasPrice: 25000000000,
+        // blockGasLimit 8000000
+    },
+    // BSC mainnet not tested yet, not sure if it works
+    bsc_mainnet: {
+        url: 'https://bsc-dataseed1.binance.org:443',
+        accounts: project_secret.private_key_list,
+        chainId: 56,
+        // 5 Gwei
+        gasPrice: 5000000000,
+        // blockGasLimit 8000000
+    },
+    */
     // Useful for private networks
     // private: {
-      // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
-      // network_id: 2111,   // This network is yours, in the cloud.
-      // production: true    // Treats this network as if it was a public net. (default: false)
+    // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
+    // network_id: 2111,   // This network is yours, in the cloud.
+    // production: true    // Treats this network as if it was a public net. (default: false)
     // }
   },
 
